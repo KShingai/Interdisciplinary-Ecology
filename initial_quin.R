2ac9babb3fa9ce9eaa34191b6aef1349857fcb2c
Quin Kyoko Shingai <quin.shingai@gmail.com>
2024-11-14 10:56:20 -0500

    Reclassified vetted column "maybe" to "yes" or "no".

--- initial_quin.R
+++ initial_quin.R
@@ -42,5 +42,75 @@ metagear::abstract_screener(file=file.choose("lit_search.csv"),
 
 
 
-##combining
+##combining and writing to csv
 df_quin <- rbind(effort_quin, effort_quindelyn, effort_quinifer)
+df_quin <- write.csv(df_quin, "df_quin.csv")
+
+##exploring combined files for everyone 
+
+##ID change from maybe to NO
+4
+11
+16
+53
+92
+111
+316
+468
+546
+76
+212
+239
+273
+467
+713
+128
+154
+254
+313
+457
+472
+473
+482
+557
+634
+
+
+##ID change from maybe to YES
+2
+585
+
+unique(combined_screen$REVIEWERS)
+
+combined_file_quin <- combined_screen %>% dplyr::filter(REVIEWERS == c("quin", "quinifer","quindelyn"))
+
+
+combined_file_edited <- combined_file_quin%>%  
+  mutate(INCLUDE = case_when( STUDY_ID %in% c(4,
+                                              11,
+                                              16,
+                                              53,
+                                              92,
+                                              111,
+                                              316,
+                                              468,
+                                              546,
+                                              76,
+                                              212,
+                                              239,
+                                              273,
+                                              467,
+                                              713,
+                                              128,
+                                              154,
+                                              254,
+                                              313,
+                                              457,
+                                              472,
+                                              473,
+                                              482,
+                                              557,
+                                              634) ~ "NO",  TRUE ~ INCLUDE))
+
+combined_file_edited_final_quin <- combined_file_edited %>%  
+  mutate(INCLUDE = case_when( STUDY_ID %in% c(2, 585) ~ "YES",  TRUE ~ INCLUDE))
